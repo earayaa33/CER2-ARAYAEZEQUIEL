a5ae030949c79181f94cf01ebff90526fcfd2237
earayaa33 <earayaa@usm.cl>
2024-10-27 23:43:23 -0300

    Version final v7

--- TiendaVerde/core/forms.py
+++ TiendaVerde/core/forms.py
@@ -10,4 +10,23 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ['username', 'first_name', "last_name", "email", "password1", "password2"]
         help_texts = { k : "" for k in fields}
 
+    def clean_user(self):
+        username = self.cleaned_data.get('username')
+        if not user:
+            raise forms.ValidationError("El usuario es obligatorio")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not email:
+            raise forms.ValidationError("El email es obligatorio.")
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Las contraseñas no coiciden.")
+        return password2
+
 
